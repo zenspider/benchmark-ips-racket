973746d0efad321dd720b77f97b7793fca7b4c30
Ryan Davis <ryand-ruby@zenspider.com>
2016-12-09 14:45:02 -0800

    added

---
+++ info.rkt
@@ -0,0 +1,10 @@
+#lang info
+
+(define version "1.0.0")
+
+(define collection "benchmark-ips")
+
+(define deps '("base"
+               "rackunit-lib"))
+
+(define build-deps '("racket-doc" "scribble-lib" "racket-doc"))
